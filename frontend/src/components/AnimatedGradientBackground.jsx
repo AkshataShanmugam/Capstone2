8f60ab93a5da9a152be6e0baa42328fc32744fb9
CelestinFernandes <celestin.fernandes21@st.niituniversity.in>
2024-11-28 17:06:06 +0530

    AnimatedGradientBackground.jsx

--- frontend/src/components/AnimatedGradientBackground.jsx
+++ frontend/src/components/AnimatedGradientBackground.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const AnimatedGradientBackground = ({ children }) => {
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
-      <div className="absolute inset-0 bg-gradient-to-br from-[#881FFF] via-[#ff814b] to-[#7e4bff] bg-[length:200%_200%] animate-gradient">
-        <div className="absolute inset-0 bg-[url('/movie-reel.svg')] opacity-5 bg-repeat"></div>
+      <div className="absolute inset-0 bg-gradient-to-br from-[#881FFF] via-[#36312f] to-[#7e4bff] bg-[length:200%_200%] animate-gradient">
       </div>
       <div className="relative z-10">
         {children}
