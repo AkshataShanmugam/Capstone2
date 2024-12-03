311e70d0e142dbe8f24f451cd052b311fa3a0964
CelestinFernandes <celestin.fernandes21@st.niituniversity.in>
2024-12-04 00:51:28 +0530

    Update package.json, package-lock.json, and server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const cors = require('cors'); // Import CORS
 const scriptRoutes = require('./routes/movieRoutes');
 const authRoutes = require('./routes/auth');
 const cookieParser = require('cookie-parser');
+const wishlistRoutes = require('./routes/wishlist');
+
 require('dotenv').config();
 
 console.log(process.env); 
@@ -24,6 +26,7 @@ mongoose.connect(process.env.MONGO_URI) // Connect to MongoDB
 
 app.use('/api/scripts', scriptRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/wishlist', wishlistRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
