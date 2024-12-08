aa0677ae49aa84dfb29c0d5ff56c53ac4c8e4dd5
AkshataShanmugam <81082573+AkshataShanmugam@users.noreply.github.com>
2024-12-08 10:25:20 +0530

    Update example1.bat - Removing API keys

--- Jenkins/example1.bat
+++ Jenkins/example1.bat
@@ -1,17 +1,18 @@
+
 @echo off
 REM Stop and clean up any existing containers
 docker-compose down
 
 REM Set up environment for Model services
 cd model\google_search
-echo SERP_API_KEY=48f5dd2aef570480a460533ece71ef9833ccbaca3fa1e8ef5721168ca6f94e96 >> .env 
-echo GROC_API_KEY=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 >> .env
+echo SERP_API_KEY=<your_serpapi_key> >> .env 
+echo GROQ_API_KEY=<YOUR_GROQ_APIKEY> >> .env
 cd ..\youtube_comment_summary
-echo YOUTUBE_API_KEY=AIzaSyBmXn2FOp806pR9zsWJOSxYeF5rgMx6UQA >> .env
-echo GROQ_API_KEY=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 >> .env
-echo HUGGINGFACE_API_KEY=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 >> .env
+echo YOUTUBE_API_KEY=<your_youtubev3_apikey> >> .env
+echo GROQ_API_KEY=<YOUR_GROQ_APIKEY> >> .env
+echo HUGGINGFACE_API_KEY=<YOUR_HF_API_KEY> >> .env
 cd ..\chat_with_ai
-echo GROC_API_KEY=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 >> .env
+echo GROC_API_KEY=<YOUR_GROQ_APIKEY> >> .env
 cd ..
 
 REM Start model services in detached mode
@@ -19,7 +20,7 @@ docker-compose up -d --build
 
 REM Set up environment for Backend services
 cd ..\backend
-echo MONGO_URI=mongodb+srv://username1:eWJeQrOM4OdkU9Oi@cluster0.dpbpr.mongodb.net/?retryWrites=true&w=majority >> .env
+echo MONGO_URI=<YOUR_MONGO_URI> >> .env
 echo PORT=5000 >> .env
 
 REM Start backend services in detached mode
